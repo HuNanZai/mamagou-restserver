e8775c9988a8d195de9b4275b780adebde5c540c
HuNanZai <123165587@qq.com>
2015-04-13 17:04:43 +0800

    add some files

---
+++ application/controllers/api/Specialsale.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-4-13
+ * Time: 上午9:49
+ */
+
+/**
+ * Class Specialsale
+ * 专场相关的数据的操作
+ */
+class Specialsale extends REST_Controller{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    function multi(){
+
+    }
+
+    function single(){
+
+    }
+}
