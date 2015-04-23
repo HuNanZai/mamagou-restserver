affe5deae3093f12b4126bf3700dd2145e33a126
HuNanZai <123165587@qq.com>
2015-04-23 11:42:29 +0800

    import the basic source files into the project

--- application/controllers/api/Specialsale.php
+++ application/controllers/api/Specialsale.php
@@ -14,13 +14,53 @@ class Specialsale extends REST_Controller{
 
     function __construct(){
         parent::__construct();
+
+        $this->load->model('specialsale_model');
     }
 
     function multi(){
 
-    }
+	}
 
     function single(){
-
+		if( $this->get('id') ){
+			$this->_single_get();
+		}
     }
+
+	/**
+	 * 单个的获取
+	 *
+	 */
+	function _single_get(){
+		if( !$this->get('id') ){
+			$this->response(null, 404);
+		}
+		$specisalsale = @$this->specialsale_model->get( $this->get('id') );
+		$this->response($specisalsale, 200);
+	}
+
+	/**
+	 * 单个更新
+	 *
+	 */
+	function _single_post(){
+
+	}
+
+	/**
+	 * 单个删除
+	 *
+	 */
+	function _single_delete(){
+
+	}
+
+	/**
+	 * 单个新增
+	 *
+	 */
+	function _single_put(){
+
+	}
 }
---
+++ application/models/specialsale_model.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 15-4-23
+ * Time: 上午10:59
+ */
+class Specialsale_model extends MY_Model {
+    public $_table      = 'special_sale';
+    public $primary_key = 'special_sale_id';
+
+
+}
